05e65fe5cee81183c56ba6109688b46dc5440a16
Max N <maksim.naranovich@kaseya.com>
2025-06-09 21:28:42 +0200

    MySql config

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <html lang="EN">
 <?php
-    phpinfo();
+$dsn = 'mysql:host=db;dbname=my_database;charset=utf8mb4';
+$user = 'root';
+$password = 'password';
+
+try {
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+    echo "Connected successfully using PDO with mysqlnd";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
 </html>
